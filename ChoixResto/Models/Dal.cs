05887f7f2f2c6cdd5fdb5edef4bee3f789c167af
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-03-03 10:36:55 +0100

    Ajoutez des fichiers projet.

---
+++ ChoixResto/Models/Dal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChoixResto.Models
+{
+    public class Dal : IDal
+    {
+        private BddContext bdd;
+
+        public Dal()
+        {
+            bdd = new BddContext();
+        }
+
+        public List<Resto> ObtientTousLesRestaurants()
+        {
+            return bdd.Restos.ToList();
+        }
+
+        public void Dispose()
+        {
+            bdd.Dispose();
+        }
+    }
+}
