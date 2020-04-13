4497350b666a006c99512efea7f9b7ed1ffd1daa
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-04-13 12:09:44 +0200

    Mise en place de la Couche d'Acces au Données (Data Access Layer) DAL

---
+++ ChoixResto/Dal/DalAccess.cs
@@ -0,0 +1,18 @@
+﻿using ChoixResto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChoixResto.Dal
+{
+    public class DalAccess
+    {
+        protected ApplicationDbContext db;
+
+        public DalAccess()
+        {
+            this.db = new ApplicationDbContext();
+        }
+    }
+}
