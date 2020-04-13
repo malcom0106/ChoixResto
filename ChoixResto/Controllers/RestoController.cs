97dd95060ee7389604c818cff0e6349820d488ef
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-04-13 14:51:42 +0200

    Creation des controller Sondage, vote et resto

---
+++ ChoixResto/Controllers/RestoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChoixResto.Models;
+
+namespace ChoixResto.Controllers
+{
+    public class RestoController : Controller
+    {
+        // GET: Restos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
